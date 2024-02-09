95162dffec838bc31c3018b417cfc12b2ec6a5d2
LKJUnity3 <dlrhkdwo2422@gmail.com>
2024-02-09 18:30:23 +0900

    ADD - TEST
    TEST

---
+++ Assets/Scripts/EnemyTEST.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyTEST : MonoBehaviour
+{
+    int HP = 10;
+
+    private void Update()
+    {
+        Death();
+    }
+
+    private void Death()
+    {
+        if (HP > 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
