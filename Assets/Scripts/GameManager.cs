644c7df7e56289893af75f3d2a920ca8c5c4a4bd
shw0514 <gusdnddl50@gmail.com>
2024-02-12 17:23:39 +0900

    FIXED - GameManager
    GameManager.cs 변경

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,11 +13,13 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI ownedGoldText;
     [SerializeField] private TextMeshProUGUI killScoreText;
 
-    [Header("GameOverMenu")]
+    [Header("Game Over Menu")]
+    [SerializeField] private GameObject gameOverUI;
     [SerializeField] private TextMeshProUGUI gameOver_curKillScore;
     [SerializeField] private TextMeshProUGUI gameOver_bestKillScore;
 
-    [Header("GameClearMenu")]
+    [Header("Game Clear Menu")]
+    [SerializeField] private GameObject gameClearUI;
     [SerializeField] private TextMeshProUGUI gameClear_curKillScore;
     [SerializeField] private TextMeshProUGUI gameClear_bestKillScore;
 
@@ -32,7 +36,6 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        curTILText.text = curTIL.ToString();
         killScoreText.text = killScore.ToString();
         ownedGoldText.text = ownedGold.ToString();
         curTIL = 0;
@@ -40,7 +43,7 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        
+        curTILText.text = curTIL.ToString();
     }
 
     public void AddTIL()
@@ -55,11 +58,23 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
-
+        Time.timeScale = 0.0f;
+        gameOverUI.SetActive(true);
     }
 
     public void GameClear()
     {
+        Time.timeScale = 0.0f;
+        gameClearUI.SetActive(true);
+    }
 
+    public void RetryGame()
+    {
+        SceneManager.LoadScene("SHWScene");
+    }
+
+    public void GoToTitle()
+    {
+        SceneManager.LoadScene("SHWScene");
     }
 }
