dc72f2498997043ec8905210749ede98ec67ce92
LKJUnity3 <dlrhkdwo2422@gmail.com>
2024-02-13 11:08:03 +0900

    Add - MoveBullet.cs

--- Assets/Scripts/MoveBullet.cs
+++ Assets/Scripts/MoveBullet.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class MoveBullet : MonoBehaviour
@@ -8,13 +9,30 @@ public class MoveBullet : MonoBehaviour
     private Vector2 sprite;
     [SerializeField]
     private Vector2 spawn;
-
+    [SerializeField]
+    private Vector2 moveDirection = Vector2.zero;
     private void Rad()
     {
         Vector2 newDirection = sprite - spawn;
         newDirection = newDirection.normalized;
+        moveDirection = newDirection;
+    }
+
+    private void Awake()
+    {
+        Rad();
+    }
+
+    private void FixedUpdate()
+    {
+        Move(moveDirection);
+    }
 
 
+
+    private void Move(Vector2 direction)
+    {
+        transform.position += new Vector3(direction.x, direction.y,0) * Time.fixedDeltaTime;
     }
 
 }
