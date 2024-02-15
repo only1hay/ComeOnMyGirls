41c51f1e9a9433f6fc7d89b65be8bd2f08fc7183
LKJUnity3 <dlrhkdwo2422@gmail.com>
2024-02-15 21:13:44 +0900

    Fixed - Upgrade

--- Assets/Scripts/Money/upgrade.cs
+++ Assets/Scripts/Money/upgrade.cs
@@ -8,6 +8,7 @@ public class upgrade : MonoBehaviour
     public TMP_Text money;
     public TMP_Text upgradePrice; //tower 각각의 price를 가져와야 함.
     private GameObject towerPosition;
+    private TowerAbility _ability;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +19,8 @@ public class upgrade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        towerPosition = GameManager.instance.GetTowerPosition();
+        upgradePrice.text = (towerPosition.transform.Find("Tower(Clone)").GetComponent<TowerAbility>().bulletDamage * 500).ToString();
     }
 
     public void UpgradeGirls()
@@ -33,7 +35,7 @@ public class upgrade : MonoBehaviour
             TowerUpgrade();
         }
 
-        upgradePrice.text = intUpgradePrice.ToString();
+        
 
         //타워랑 연결시켜 타워의 능력치 등을 변화시켜줘야 함.
     }
